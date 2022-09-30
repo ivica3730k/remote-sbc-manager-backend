6cfab090336a18d72339787160fcc7a984c75468
Ivica Matic <ivica-matic@outlook.com>
2022-09-30 22:39:47 +0100

    Added list files service

--- app/main/controller/file_controller.py
+++ app/main/controller/file_controller.py
@@ -27,3 +27,27 @@ class FileUpload(Resource):
             return {"message": f"Saved file {result}"}, 200
         except FileAlreadyExistsError as e:
             return {"message": str(e)}, 409
+
+    @api.doc(description="Get all stored files")
+    @api.response(200, "Files retrieved successfully")
+    def get(self):
+        return get_stored_files(), 200
+
+
+@api.route('/iso')
+class FileUpload(Resource):
+
+    @api.doc(description="Upload file")
+    @api.response(200, "File stored successfully")
+    @api.response(409, "File already exists")
+    @api.expect(FileDto.upload_parser)
+    def post(self):
+        args = FileDto.upload_parser.parse_args()
+        file = args["file"]
+
+        filename = secure_filename(file.filename)
+        try:
+            result = save_file(file, filename, "iso")
+            return {"message": f"Saved file {result}"}, 200
+        except FileAlreadyExistsError as e:
+            return {"message": str(e)}, 409
--- app/main/service/file_service.py
+++ app/main/service/file_service.py
@@ -1,13 +1,17 @@
 from flask import current_app
 from ..custom_exceptions import *
+from collections import defaultdict
+import glob
+import json
 import os
+from os import path
 
 
-def save_file(file, filename, folder=""):
-    if folder:
-        folder = f"./uploads/{folder}"
+def save_file(file, filename, folder=None):
+    if folder is not None:
+        folder = f"./files/{folder}"
     else:
-        folder = "./uploads"
+        folder = "./files/uploads"
     if not os.path.exists(folder):
         os.makedirs(folder)
     filepath = f"{folder}/{filename}"
@@ -19,3 +23,14 @@ def save_file(file, filename, folder=""):
 
 def _does_file_exist(filepath):
     return os.path.exists(filepath)
+
+
+def get_stored_files():
+    base_path = "./files"
+    d = defaultdict(list)
+
+    for file in glob.iglob(path.join(base_path, '*/*'), recursive=True):
+        d[path.basename(path.dirname(file))].append(path.basename(file))
+
+    return d
+
